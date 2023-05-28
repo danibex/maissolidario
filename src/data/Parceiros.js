cbd9d5108e590055af1ae1ad8954afbf9e474430
danibex <danielivam96@gmail.com>
2023-05-28 00:01:40 -0300

    .

--- src/data/Parceiros.js
+++ src/data/Parceiros.js
@@ -1,128 +1,43 @@
-import Universo from "@/assets/img/Parceiros/Universo.png"
-import Atualiza from "@/assets/img/Parceiros/Atualiza.png"
-import Batista from "@/assets/img/Parceiros/Batista.png"
-import Cairu from "@/assets/img/Parceiros/Cairu.png"
-import ColegioAcademico from "@/assets/img/Parceiros/ColegioAcademico.png"
-import CPET from "@/assets/img/Parceiros/CPET.png"
-import DimensaoMax from "@/assets/img/Parceiros/DimensaoMax.png"
-import EduTec from "@/assets/img/Parceiros/EduTec.png"
-import Fatec from "@/assets/img/Parceiros/Fatec.png"
-import SantissimoSacramento from "@/assets/img/Parceiros/SantissimoSacramento.png"
-import Unex from "@/assets/img/Parceiros/Unex.png"
-import UniESPG from "@/assets/img/Parceiros/UniESPG.png"
-import UniFatecie from "@/assets/img/Parceiros/UniFatecie.png"
-import Uniftc from "@/assets/img/Parceiros/Uniftc.png"
-
-export const parceiros = [
-    {
-        id: 1,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: Universo,
-        link: ""
-    },
-    {
-        id: 2,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: Atualiza,
-        link: ""
-    },
-    {
-        id: 3,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: Batista,
-        link: ""
-    },
-    {
-        id: 4,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: Cairu,
-        link: ""
-    },
-    {
-        id: 5,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: ColegioAcademico,
-        link: ""
-    },
-    {
-        id: 6,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        URLescudo: CPET,
-        link: ""
-    },
-    {
-        id: 7,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: DimensaoMax,
-        link: ""
-    },
-    {
-        id: 8,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: EduTec,
-        link: ""
-    },
-    {
-        id: 9,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: Fatec,
-        link: ""
-    },
-    {
-        id: 10,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: SantissimoSacramento,
-        link: ""
-    },
-    {
-        id: 11,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: Unex,
-        link: ""
-    },
-    {
-        id: 12,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: UniESPG,
-        link: ""
-    },
-    {
-        id: 13,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: UniFatecie,
-        link: ""
-    },
-    {
-        id: 14,
-        porcentagemMaxima: "Bolsas de até 80%",
-        menorPreco: "A partir de R$ 399,00",
-        width: "",
-        URLescudo: Uniftc,
-        link: ""
+class Parceiro {
+    constructor(id, porcentagemMaxima, menorPreco, URLescudo, link, width = "") {
+        this.id = id;
+        this.porcentagemMaxima = porcentagemMaxima;
+        this.menorPreco = menorPreco;
+        this.URLescudo = URLescudo;
+        this.link = link;
+        this.width = width;
     }
-]
+}
+  
+  const Universo = require("@/assets/img/Parceiros/Universo.png");
+  const Atualiza = require("@/assets/img/Parceiros/Atualiza.png");
+  const Batista = require("@/assets/img/Parceiros/Batista.png");
+  const Cairu = require("@/assets/img/Parceiros/Cairu.png");
+  const ColegioAcademico = require("@/assets/img/Parceiros/ColegioAcademico.png");
+  const CPET = require("@/assets/img/Parceiros/CPET.png");
+  const DimensaoMax = require("@/assets/img/Parceiros/DimensaoMax.png");
+  const EduTec = require("@/assets/img/Parceiros/EduTec.png");
+  const Fatec = require("@/assets/img/Parceiros/Fatec.png");
+  const SantissimoSacramento = require("@/assets/img/Parceiros/SantissimoSacramento.png");
+  const Unex = require("@/assets/img/Parceiros/Unex.png");
+  const UniESPG = require("@/assets/img/Parceiros/UniESPG.png");
+  const UniFatecie = require("@/assets/img/Parceiros/UniFatecie.png");
+  const Uniftc = require("@/assets/img/Parceiros/Uniftc.png");
+  
+  export const parceiros = [
+    new Parceiro(1, "Bolsas de até 80%", "A partir de R$ 399,00", Universo, ""),
+    new Parceiro(2, "Bolsas de até 80%", "A partir de R$ 399,00", Atualiza, ""),
+    new Parceiro(3, "Bolsas de até 80%", "A partir de R$ 399,00", Batista, ""),
+    new Parceiro(4, "Bolsas de até 80%", "A partir de R$ 399,00", Cairu, ""),
+    new Parceiro(5, "Bolsas de até 80%", "A partir de R$ 399,00", ColegioAcademico, ""),
+    new Parceiro(6, "Bolsas de até 80%", "A partir de R$ 399,00", CPET, ""),
+    new Parceiro(7, "Bolsas de até 80%", "A partir de R$ 399,00", DimensaoMax, ""),
+    new Parceiro(8, "Bolsas de até 80%", "A partir de R$ 399,00", EduTec, ""),
+    new Parceiro(9, "Bolsas de até 80%", "A partir de R$ 399,00", Fatec, ""),
+    new Parceiro(10, "Bolsas de até 80%", "A partir de R$ 399,00", SantissimoSacramento, ""),
+    new Parceiro(11, "Bolsas de até 80%", "A partir de R$ 399,00", Unex, ""),
+    new Parceiro(12, "Bolsas de até 80%", "A partir de R$ 399,00", UniESPG, ""),
+    new Parceiro(13, "Bolsas de até 80%", "A partir de R$ 399,00", UniFatecie, ""),
+    new Parceiro(14, "Bolsas de até 80%", "A partir de R$ 399,00", Uniftc, "")
+];
+  
