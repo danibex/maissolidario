a4406bebef2358f8a29a290ffd9feb0787f64658
danibex <danielivam96@gmail.com>
2023-05-29 08:02:50 -0300

    ultimas alteracoe antes da reuniao

--- src/data/PrincipaisCursosEad.js
+++ src/data/PrincipaisCursosEad.js
@@ -10,16 +10,21 @@ class Curso {
     }
 }
   
-  const direito = require("@/assets/img/Cursos/direito.png");
+  const pedagogia = require("@/assets/img/Cursos/pedagogia.png");
+  const administracao = require("@/assets/img/Cursos/administracao.png");
+  const logistica = require("@/assets/img/Cursos/logistica.png");
+  const assistenteSocial = require("@/assets/img/Cursos/assistenteSocial.png");
+  const gastronomia = require("@/assets/img/Cursos/gastronomia.png");
+  const marketing = require("@/assets/img/Cursos/marketing.png");
 
 
   
   export const PrincipaisCursosEad = [
-    new Curso(1, "Medicina", 1223.64, 80, 460, 4537.62, direito),
-    new Curso(2, "Medicina", 1223.64, 80, 460, 4537.62, direito),
-    new Curso(3, "Medicina", 1223.64, 80, 460, 4537.62, direito),
-    new Curso(4, "Medicina", 1223.64, 80, 460, 4537.62, direito),
-    new Curso(5, "Medicina", 1223.64, 80, 460, 4537.62, direito),
-    new Curso(6, "Medicina", 1223.64, 80, 460, 4537.62, direito)
+    new Curso(1, "Pedagogia", 1223.64, 80, 460, 4537.62, pedagogia),
+    new Curso(2, "Administração", 1223.64, 80, 460, 4537.62, administracao),
+    new Curso(3, "Logística", 1223.64, 80, 460, 4537.62, logistica),
+    new Curso(4, "Serviço Social", 1223.64, 80, 460, 4537.62, assistenteSocial),
+    new Curso(5, "Gastronomia", 1223.64, 80, 460, 4537.62, gastronomia),
+    new Curso(6, "Marketing", 1223.64, 80, 460, 4537.62, marketing)
   ];
   
