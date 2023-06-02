2e25bac1eee814d7afd1726a482cf144abfdb3e4
danibex <danielivam96@gmail.com>
2023-06-02 12:27:18 -0300

    Links Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,32 @@
+import Link from "next/link"
 export default function Footer() {
 return(
     <div className="bg-green-400 w-[100%] h-auto p-4 shadow-lg text-slate-700 flex justify-center items-center flex-col">
         <div className="flex sm:flex-row flex-col justify-around items-center w-full sm:w-[80%]">
             <div className="h-[200px] flex flex-col justify-center items-center">
-                <h1 className="mb-2 text-xl font-bold">Contatos</h1>
+                <h2 className="mb-2 text-xl font-bold">Contatos</h2>
                 <div className="flex flex-col justify-center items-center">
-                    <p>contato@maissolidario.com.br</p>
-                    <p>maissolidarioadm@gmail.com</p>
-                    <p>(71) 9 9259-2363</p>
-                    <p>(71) 9 8761-2190</p>
+                    <Link href="/" className="hover:underline hover:text-slate-200 active:text-slate-400">contato@maissolidario.com.br</Link>
+                    <Link href="/" className="hover:underline hover:text-slate-200 active:text-slate-400">maissolidarioadm@gmail.com</Link>
+                    <Link href="/" className="hover:underline hover:text-slate-200 active:text-slate-400">(71) 9 9259-2363</Link>
+                    <Link href="/" className="hover:underline hover:text-slate-200 active:text-slate-400">(71) 9 8761-2190</Link>
                 </div>
             </div>
             <div className="h-[150px] flex flex-col justify-center items-center">
-                <h1 className="mb-2 text-xl font-bold">Bolsas de Estudo</h1>
+                <h2 className="mb-2 text-xl font-bold">Bolsas de Estudo</h2>
                 <div className="flex flex-col justify-center items-center">
-                    <p>Bolsas</p>
-                    <p>Graduação</p>
-                    <p>Pós-Graduação</p>
-                    <p>Técnico</p>
-                    <p>Colégio</p>
+                <Link href="/bolsas" className="hover:underline hover:text-slate-200 active:text-slate-400">Bolsas</Link>
+                <Link href="/bolsas" className="hover:underline hover:text-slate-200 active:text-slate-400">Graduação</Link>
+                <Link href="/bolsas" className="hover:underline hover:text-slate-200 active:text-slate-400">Pós-Graduação</Link>
+                <Link href="/bolsas" className="hover:underline hover:text-slate-200 active:text-slate-400">Técnico</Link>
+                <Link href="/bolsas" className="hover:underline hover:text-slate-200 active:text-slate-400">Colégio</Link>
                 </div> 
             </div>
             <div className="h-[150px] flex flex-col justify-center items-center">
-                <h1 className="mb-2 text-xl font-bold">Links Úteis</h1>
+                <h2 className="mb-2 text-xl font-bold">Links Úteis</h2>
                 <div className="flex flex-col justify-center items-center">
-                    <p>Atendimento</p>
-                    <p>Blog</p>
+                <Link href="/" className="hover:underline hover:text-slate-200 active:text-slate-400">Atendimento</Link>
+                <Link href="/" className="hover:underline hover:text-slate-200 active:text-slate-400">Blog</Link>
                 </div>
             </div>
         </div>
