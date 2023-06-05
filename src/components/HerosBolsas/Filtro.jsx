f1ccd1020869944385218261c7644cb7e6f27f26
danibex <danielivam96@gmail.com>
2023-06-05 20:33:42 -0300

    ultimas atualizações 05/06

--- src/components/HerosBolsas/Filtro.jsx
+++ src/components/HerosBolsas/Filtro.jsx
@@ -1,7 +1,22 @@
 export default function Filtro(props){
 return(
-<div>
-    {props.children}
+<div className="flex-col w-full border-blue-800 border h-[240px] rounded-b-xl flex justify-center items-center">
+    <div className="flex flex-row justify-around w-[100%]">
+        <div>
+            <p>Digite o Nome do Curso:</p>
+            <input type="text" placeholder="Teste"/>
+        </div>
+        <div>
+            <p>Nome da Faculdade (opcional):</p>
+            <input type="text" placeholder="Teste"/>
+        </div>
+        <div>
+            <p>Cidade:</p>
+            <input type="text" placeholder="Teste"/>
+        </div>
+        <button>Procurar</button>
+    </div>
+    filtro
 </div>
 )
 }
