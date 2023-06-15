8f4cecc506f1a9364ff5fcf8f179b57b2382a211
danibex <danielivam96@gmail.com>
2023-06-15 20:47:24 -0300

    .

--- src/pages/api/parceiros/[teste].js
+++ src/pages/api/parceiros/[teste].js
@@ -5,7 +5,8 @@ export default async function handler(req, res) {
         try {
             const parceiros = await listarParceiros();
             res.status(200).json(parceiros);
-            } catch (error) {
+            } 
+        catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Ocorreu um erro no servidor' });
             }
