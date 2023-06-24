bab8c3bbc5e6002b09657267be795805fad19d28
danibex <danielivam96@gmail.com>
2023-06-23 22:54:56 -0300

    alterações no banco de dados

--- src/models/acess.js
+++ src/models/acess.js
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 const pool = new Pool({
   user: process.env.USER_POSTGRES,
   host: process.env.HOST,
-  database: 'Mais Solidario',
+  database: 'mais_solidario',
   password: '323019',
   port: 5432,
 });
