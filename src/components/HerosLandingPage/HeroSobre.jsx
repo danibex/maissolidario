174c618b83ae0b097fdedb6a9960c267924e376e
danibex <danielivam96@gmail.com>
2023-07-09 14:42:55 -0300

    .

--- src/components/HerosLandingPage/HeroSobre.jsx
+++ src/components/HerosLandingPage/HeroSobre.jsx
@@ -7,7 +7,7 @@ return(
 <div className={`w-full p-3 m-0 h-auto bg-[#189ED7]`}>
     <div className={`md:flex-row-reverse p-4 rounded-lg justify-center items-center flex flex-col w-full`}>
         <HeroBase className="m-2 text-center">
-            <p className="text-3xl text-slate-100 font-semibold">O programa Mais Solidário Bolsas de Estudos oferece bolsas de estudo para estudantes de baixa renda, permitindo que tenham acesso à educação de qualidade e oportunidades de desenvolvimento.</p>
+            <p className="text-3xl text-slate-100 font-semibold">O programa Mais Solidário Bolsas oferece bolsas de estudo para estudantes de baixa renda, permitindo que tenham acesso à educação de qualidade e oportunidades de desenvolvimento.</p>
         </HeroBase>
         <HeroBase className="m-2">  
             <Image src="/images/jovemComLivro.jpg" width={600} height={200} className="rounded-xl" alt="Estudante apontando para texto"/>
--- src/models/acess.js
+++ src/models/acess.js
@@ -3,8 +3,8 @@ import { Pool } from 'pg';
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'mais_solidario',
-  password: '323019',
+  database: 'mais_solidario', // alterar para maissolidariodb
+  password: '323019', // alterar para Maissolidario20....
   port: 5432,
 });
 
